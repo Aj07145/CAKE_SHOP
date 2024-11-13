543b9fb29d4c980f5801ed1e244efbf6225897de
Aj07145 <arskv7145@gmail.com>
2024-11-13 10:23:35 +0530

    update

--- Cake-shop-A2-main/src/index.js
+++ Cake-shop-A2-main/src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <HashRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </HashRouter>,
-  document.getElementById('root')
+  </React.StrictMode>
+
 );
+
 
 
 // If you want to start measuring performance in your app, pass a function
