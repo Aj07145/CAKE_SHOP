e03620654ddc65e4956ddccef0866b9f4c71a093
Aj07145 <arskv7145@gmail.com>
2024-11-13 08:48:02 +0530

    update

---
+++ Cake-shop-A2-main/src/pages/About.js
@@ -0,0 +1,28 @@
+// src/pages/About.js
+import React from 'react';
+import './About.css';
+
+const About = () => (
+  <div className="about-page">
+    <h2>All you need to know about our shop</h2>
+    <p>
+    Step into a world of exquisite flavors and stunning designs.
+     AJ cakes invites you to experience the epitome of cake artistry. 
+     Our handcrafted cakes are a masterpiece, perfect for any occasion
+    </p>
+    <p>
+    Our mission is to bake joy, one slice at a time. We believe that every cake is a canvas, 
+    and every bite is a masterpiece. We blend love, artistry, and the finest ingredients to create cakes that not only taste incredible
+     but also bring smiles to faces.
+    </p>
+    <h3>Contact Us</h3>
+    <p>Phone: 7994449688</p>
+    <p>Email: info@cakeeshop.com</p>
+    <h3>Follow Us on Social Media</h3>
+    <div className="social-links">
+       <a href="https://www.instagram.com/10_arjun_01">Instagram</a> | <a href="https://x.com/ArjunKV7546188">Twitter</a>
+    </div>
+  </div>
+);
+
+export default About;
